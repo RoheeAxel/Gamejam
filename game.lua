fece577e1d0865a6d9171d83c21765ff9c8f1d6a
thomas <thomas.juin@epitech.eu>
2023-03-25 12:12:49 -0400

    feat (Music) : addign chrono when gas gas gas

--- game.lua
+++ game.lua
@@ -3,6 +3,10 @@ function draw_game(frame)
     love.graphics.draw(Background[frame], 1168 * 2, 0, 0, -1, 1)
     myMap.Draw(currentMap[1], 304, 112)
     myMap.Draw(currentMap[2], 1280/2 + 304, 112)
+    if Musics.time ~= 0 then
+        Musics.actualtime = time - Musics.time
+        love.graphics.print("Time : " .. (math.floor(Musics.actualtime / 60)) .. ":" .. (math.floor(Musics.actualtime % 60)), 1100, 10)
+    end
 end
 
 function game_keypressed(key)
@@ -11,7 +15,7 @@ function game_keypressed(key)
     end
     updatePlayer(player1, key, currentMap[1], 1)
     updatePlayer(player2, key, currentMap[2], 2)
-    updateMusic(Musics, key)
+    updateMusic(Musics, key, time)
 end
 
 function update_game(dt)
