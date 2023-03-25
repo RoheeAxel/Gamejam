e6159ea5cf6070b6cc8b7d9fc6f8d56baa63a7c4
Sinan Karakaya <sinan.karakaya@epitech.eu>
2023-03-25 14:59:12 +0100

    feat: loading bar + new background

--- frame_getter.py
+++ frame_getter.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import numpy as np
 import cv2
 from glob import glob
@@ -33,7 +34,7 @@ def save_frame(video_path, save_dir, gap=10):
         idx += 1
 
 if __name__ == "__main__":
-    video_paths = glob("Assets/cyberpunk.gif")
+    video_paths = glob(sys.argv[1])
     save_dir = "Assets/frames"
 
     for path in video_paths:
--- main.lua
+++ main.lua
@@ -11,20 +11,34 @@ function love.load()
     currentMap[1] = myMap.generateMap("Maps/Lvl1/map", 1)
     currentMap[2] = myMap.generateMap("Maps/Lvl1/map", 1)
     separatedTileSet = myMap.makeQuad()
-    --Background
+    -- Background
     Background = {}
-    for i = 1, 33 do
-        Background[i] = love.graphics.newImage("Assets/Background/cyberpunk/" .. i - 1 .. ".png")
-    end
+    -- for i = 1, 448 / 4, 2 do
+    --     Background[i] = love.graphics.newImage("Assets/frames/background-cyberpunk/" .. i - 1 .. ".png")
+    -- end
     time = 0
-
+    isLoaded = 1
 end
 
 function love.draw()
     local frame = math.floor(time * 10) % 33 + 1
-    love.graphics.draw(Background[frame], 0, 0,0,0.67,0.665)
-    myMap.Draw(currentMap[1], 304, 112)
-    myMap.Draw(currentMap[2], 1280/2 + 304, 112)
+
+    if isLoaded == 448 / 4 then
+        love.graphics.draw(Background[frame], 0, 0,0,1,1)
+        love.graphics.draw(Background[frame], 1168 * 2, 0, 0, -1, 1)
+        myMap.Draw(currentMap[1], 304, 112)
+        myMap.Draw(currentMap[2], 1280/2 + 304, 112)
+    elseif isLoaded ~= 448 / 4 then
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.rectangle("fill", 320, 720 / 2 + 100, 640, 15)
+        love.graphics.setColor(236 / 256, 89 / 256, 233 / 256)
+        love.graphics.rectangle("fill", 320, 720 / 2 + 100, 640 * (isLoaded / (448 / 4)), 15)
+    end
+
+    -- love.graphics.draw(Background[frame], 0, 0,0,1,1)
+    -- love.graphics.draw(Background[frame], 1168 * 2, 0, 0, -1, 1)
+    -- myMap.Draw(currentMap[1], 304, 112)
+    -- myMap.Draw(currentMap[2], 1280/2 + 304, 112)
 end
 
 function love.keypressed(key)
@@ -34,4 +48,8 @@ end
 
 function love.update(dt)
     time = time + dt
+    if isLoaded ~= 448 / 4 then
+        Background[isLoaded] = love.graphics.newImage("Assets/frames/background-cyberpunk/" .. isLoaded - 1 .. ".png")
+        isLoaded = isLoaded + 1
+    end
 end
